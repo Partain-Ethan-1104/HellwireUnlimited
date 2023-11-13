9ee22f67465f62c164756633e260b316e5c5df7a
BattlePants409 <73601509+BattlePants409@users.noreply.github.com>
2023-11-12 18:00:35 -0800

    bullet lifespan

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -7,7 +7,26 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public GameObject hitEffect;
+    public float lifespan = 2.0f; // Adjust the lifespan as needed
+
+    private float timer = 0.0f;
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= lifespan)
+        {
+            DestroyBullet();
+        }
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
+    {
+        DestroyBullet();
+    }
+
+    private void DestroyBullet()
     {
         GameObject effect = Instantiate(hitEffect, transform.position, quaternion.identity);
         Destroy(effect, 0.2f);
